0adc895e317c608cd38f40dda4d2dec7e1cb857a
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-18 16:51:28 +0300

    login.feature

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,7 +1,9 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 /*
 In the class we will be able to pass pre- & pos- conditions to each scenario and each step
@@ -17,6 +19,17 @@ public class Hooks {
     @After
     public void teardownScenario(){
         System.out.println("===Closing browser using cucumber @After");
+        System.out.println("===Scenario ended/ Take screenshot if failed!");
+    }
+
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("-------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("------> applying tearDown @AfterStep");
     }
 
 }
