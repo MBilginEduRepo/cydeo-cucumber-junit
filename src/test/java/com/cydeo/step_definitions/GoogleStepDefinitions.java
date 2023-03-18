5af302d95e19c6810eb8acbc01ff3ee9abd5d2b9
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-18 20:03:05 +0300

    login.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,11 +1,30 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage= new GoogleSearchPage();
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle="apple - Google Search";
+        String actualTitle= Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals(expectedTitle,actualTitle);
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
+    }
+
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         System.out.println("user is on Google search page");
@@ -24,13 +43,6 @@ public class GoogleStepDefinitions {
 
     }
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
 
-    }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-
-    }
 
 }
