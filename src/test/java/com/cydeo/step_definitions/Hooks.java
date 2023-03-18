2a0e76844e554cfff723984da3b6e997506c84db
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-18 15:33:57 +0300

    login.feature

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,16 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.Before;
+
+/*
+In the class we will be able to pass pre- & pos- conditions to each scenario and each step
+ */
+public class Hooks {
+
+    //import from io.cucumber.java not from junit
+    @Before
+    public void setupScenario(){
+        System.out.println("===Setting up browser using cucumber @Before");
+    }
+
+}
