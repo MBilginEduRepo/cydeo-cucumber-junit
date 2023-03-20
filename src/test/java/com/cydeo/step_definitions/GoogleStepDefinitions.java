f092e77be8e9dab0bb6c8952d7fbc42d6a502a65
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-20 15:43:53 +0300

    login.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,11 +13,15 @@ public class GoogleStepDefinitions {
 
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String string) {
-
+        googleSearchPage.searchBox.sendKeys(string);
     }
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
+        String expectedTitle="apple - Google'da Ara";
+        String actualTitle= Driver.getDriver().getTitle();
 
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle,actualTitle);
     }
 
     @When("user types apple and clicks enter")
@@ -31,6 +35,7 @@ public class GoogleStepDefinitions {
 
         //Junit assertion accepts first arg as expected, second arg as actual
         Assert.assertEquals("Title is not as expected!",expectedTitle,actualTitle);
+
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
