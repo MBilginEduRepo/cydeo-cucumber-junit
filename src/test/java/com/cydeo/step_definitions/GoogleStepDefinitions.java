461e8adf2fc4c0d679c0f9a42bef288f0e8cc5c9
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-20 16:54:47 +0300

    login.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -11,6 +11,11 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage= new GoogleSearchPage();
 
+    @When("user types {word} and clicks enter")
+    public void user_types_and_clicks_enter2(String searchKeyword) {
+        googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
+    }
+
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
         googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
@@ -24,13 +29,10 @@ public class GoogleStepDefinitions {
         Assert.assertEquals("Title is not as expected!",expectedTitle,actualTitle);
     }
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
-    }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-        String expectedTitle="apple - Google'da Ara";
+
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String word) {
+        String expectedTitle=word+" - Google'da Ara";
         String actualTitle= Driver.getDriver().getTitle();
 
         //Junit assertion accepts first arg as expected, second arg as actual
