03706c5e8d444d6ae9ba0e6ae0c30853366bfcc8
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-03-20 17:11:00 +0300

    login.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -11,15 +11,16 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage= new GoogleSearchPage();
 
-    @When("user types {word} and clicks enter")
-    public void user_types_and_clicks_enter2(String searchKeyword) {
-        googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter2() {
+        googleSearchPage.searchBox.sendKeys("apple"+Keys.ENTER);
     }
 
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
         googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
     }
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
         String expectedTitle=string+" - Google'da Ara";
@@ -29,10 +30,9 @@ public class GoogleStepDefinitions {
         Assert.assertEquals("Title is not as expected!",expectedTitle,actualTitle);
     }
 
-
-    @Then("user sees {word} in the google title")
-    public void user_sees_apple_in_the_google_title(String word) {
-        String expectedTitle=word+" - Google'da Ara";
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle="apple - Google'da Ara";
         String actualTitle= Driver.getDriver().getTitle();
 
         //Junit assertion accepts first arg as expected, second arg as actual
@@ -47,6 +47,7 @@ public class GoogleStepDefinitions {
 
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         System.out.println("user is seeing title is Google");
